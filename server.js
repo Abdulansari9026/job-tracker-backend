968720f850553373cb4d57e83f8259d25dd622ee
Abdul Rahman Ansari <abdulansari.engineer@gmail.com>
2025-04-09 20:51:53 +0530

    Added root route to fix 'Cannot GET /'

--- server.js
+++ server.js
@@ -12,10 +12,15 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Routes
+// Root route
+app.get('/', (req, res) => {
+  res.send('API is running 🚀');
+});
+
+// API Routes
 app.use('/api/jobs', jobRoutes);
 
-// Server start
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
